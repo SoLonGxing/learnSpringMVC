16d822f7a0fe4c0821e0a416de69a1f1f00f6de2
xingpk <xpeikang@gmail.com>
2018-04-04 09:18:43 +0800

    new

---
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,20 @@
+package spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};//配置指定的类
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0] { "/" }; //将Dispatcher映射到/路径
+    }
+}
---
+++ src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,30 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewRendererServlet;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc//启动Spring MVC
+@ComponentScan("spitter.web")  //启动组件扫描
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public ViewResolver viewResolver(){//配置JSP视图解释器
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/VIEWS/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){//配置静态资源的处理
+        configurer.enable();
+    }
+}
