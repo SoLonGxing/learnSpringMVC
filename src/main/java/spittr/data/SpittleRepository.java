b47c21857ef8a304097f7063c059cccd6b4944b2
xpk3227 <xpeikang@gmail.com>
2018-04-09 23:39:40 +0800

    new

--- src/main/java/spittr/data/SpittleRepository.java
+++ src/main/java/spittr/data/SpittleRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface SpittleRepository {
     List<Spittle> findSpittles(long max, int count);
+    String findOne(long spittleId);
 }
--- src/main/java/spittr/web/SpittleController.java
+++ src/main/java/spittr/web/SpittleController.java
@@ -2,14 +2,11 @@ package spittr.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import spittr.Spittle;
 import spittr.data.SpittleRepository;
-
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/spittles")
@@ -23,14 +20,20 @@ public class SpittleController {
         this.spittleRepository = spittleRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<Spittle> spittles(
+//            @RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
+//            @RequestParam(value = "count", defaultValue = "20") int count)
+//    {
+//        return spittleRepository.findSpittles(Long.MAX_VALUE, 20);
+//    }
 
-    public List<Spittle> spittles(
-            @RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
-            @RequestParam(value = "count", defaultValue = "20") int count)
-    {
-        return spittleRepository.findSpittles(Long.MAX_VALUE, 20);
+
+    @RequestMapping(value="/{spittleId}", method = RequestMethod.GET)
+    public String spittle (
+            @PathVariable long spittleId,  Model model)   {
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
     }
-
 
 }
